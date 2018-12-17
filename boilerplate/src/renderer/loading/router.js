8175148420b971be3d0eb2233e94880161307aed
Steven Wu <yunpeng.wu@nbltrust.com>
2018-12-17 17:18:56 +0800

    react router 4 test

--- boilerplate/src/renderer/loading/router.js
+++ boilerplate/src/renderer/loading/router.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import { Router, Switch, Route } from 'dva/router';
+import PropTypes from 'prop-types';
+import { routerRedux, Switch, Route, Redirect } from 'dva/router';
 import dynamic from 'dva/dynamic';
 
-function RouterConfig({ history, app }) {
+const { ConnectedRouter } = routerRedux;
+
+const Routers = ({ history, app }) => {
+  window.app = app;
+
   const IndexPage = dynamic({
     app,
     component: () => import('./routes/IndexPage'),
   });
 
   return (
-    <Router history={history}>
-      <Switch>
-        <Route exact path="/" component={IndexPage} />
-      </Switch>
-    </Router>
-  );
+    <ConnectedRouter history={history}>
+      <div>
+        React Rotuer Course
+        <Switch>
+          <Route exact path='/' render={() => <Redirect to='/index' />} />
+          <Route exact path='/index' component={IndexPage} />
+        </Switch>
+      </div>
+    </ConnectedRouter>
+  )
+};
+
+Routers.propTypes = {
+  history: PropTypes.object,
+  app: PropTypes.object,
 }
 
-export default RouterConfig;
+export default Routers;
